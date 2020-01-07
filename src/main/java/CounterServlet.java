820eb60c5fa3f0add7ab6761f8449dfebf524f42
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-01-07 15:20:40 -0600

    Servlets Exercise

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -6,19 +6,23 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
-public class HelloWorldServlet extends HttpServlet {
-    @Override
+@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+public class CounterServlet extends HttpServlet{
+    int pageCounter = 0;
+
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        String userName = req.getParameter("name");
+        String userResponse = req.getParameter("reset");
+        pageCounter++;
 
-        if(userName == null || userName.equals("")){
-            out.println("<h1>Hello World!</h1>");
-        }else{
-            out.println("<h1>Hello " + userName + "!</h1>");
+        if(userResponse != null && userResponse.toLowerCase().equals("yes")){
+            pageCounter = 0;
+            out.println("<p>Counter was reset</p>");
         }
 
+        out.println("<p>The Page Counter: " + pageCounter + "</p>");
     }
 }
+
