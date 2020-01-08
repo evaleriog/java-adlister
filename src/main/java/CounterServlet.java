de30ba8bef2db87c6df0ada3bdf2f2d367d04c94
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-01-08 14:07:36 -0600

    changes

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -4,11 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import javax.servlet.*;
-import javax.servlet.http.*;
 
 @WebServlet(name = "CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet{
-    int pageCounter = 0;
+    private int pageCounter = 0;
 
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -17,9 +16,12 @@ public class CounterServlet extends HttpServlet{
         String userResponse = req.getParameter("reset");
         pageCounter++;
 
-        if(userResponse != null && userResponse.toLowerCase().equals("yes")){
-            pageCounter = 0;
-            out.println("<p>Counter was reset</p>");
+        if(userResponse != null){
+            if(userResponse.equals("yes")){
+                pageCounter = 0;
+                out.println("<p>Counter was reset</p>");
+            }
+
         }
 
         out.println("<p>The Page Counter: " + pageCounter + "</p>");
